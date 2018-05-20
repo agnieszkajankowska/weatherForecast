7489087e782a15d1a3e858000ed80dff52afb15f
Agnieszka Jankowska <ajankowska0404@gmail.com>
2018-05-20 19:54:31 +0200

    Main weather icon added and search improved

--- src/App.js
+++ src/App.js
@@ -8,26 +8,26 @@ import ForecastWeatherTile from './components/ForecastWeatherTile';
 class App extends Component {
     constructor() {
         super()
-
         this.state = {
             weatherData: null,
-            city: ''
+            city: 'Gdańsk',
+            searchedCity: 'Gdańsk'
         }
 
         this.searchWeather = (e) => {
             e.preventDefault()
-            console.log(this.state.city)
-            fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + this.state.city + '&APPID=0de64b18e7da2d5a45857d165125c350')
+            this.setState({city: this.state.searchedCity})
+            fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + this.state.searchedCity + '&APPID=0de64b18e7da2d5a45857d165125c350')
                 .then(response => response.json()).then(weatherData => this.setState({weatherData: weatherData.list}))
         }
 
         this.handleChange = (e) => {
-            this.setState({city: e.target.value})
+            this.setState({searchedCity: e.target.value})
         }
     }
 
     componentDidMount() {
-        fetch('http://api.openweathermap.org/data/2.5/forecast?q=gdansk,pl&APPID=0de64b18e7da2d5a45857d165125c350')
+        fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + this.state.searchedCity + '&APPID=0de64b18e7da2d5a45857d165125c350')
             .then(response => response.json()).then(weatherData => this.setState({weatherData: weatherData.list}))
     }
 
@@ -39,8 +39,9 @@ class App extends Component {
                         <header className="App-header">
                             <h1 className="App-title">Weather app</h1>
                         </header>
+                        <h2>Current weather in {this.state.city}</h2>
                         <form onSubmit={this.searchWeather}>
-                        <input type="text" value={this.state.city} onChange={this.handleChange}/>
+                        <input type="text" value={this.state.searchedCity} onChange={this.handleChange}/>
                         <button type="submit">Search</button>
                         </form>
                         <CurrentWeatherTile weatherData={this.state.weatherData[0]}/>
--- src/components/CurrentWeatherTile.js
+++ src/components/CurrentWeatherTile.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import icons from '../icons'
+import weatherIcons from '../icons'
 import 'weathericons/css/weather-icons.css';
+
+
 
 class CurrentWeatherTile extends React.Component {
     render() {
@@ -10,9 +12,23 @@ class CurrentWeatherTile extends React.Component {
         const cloudiness = this.props.weatherData.clouds.all
         const humidity = this.props.weatherData.main.humidity
         const pressure = this.props.weatherData.main.pressure
+
+        const getWeatherIcon = () => {
+            let prefix = 'wi wi-'
+            let code = this.props.weatherData.weather[0].id
+            let icon = weatherIcons[code].icon
+
+            if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+                icon = 'day-' + icon
+            }
+
+            return icon = prefix + icon
+        }
+
+        getWeatherIcon()
         return (
             <div className="current-weather-container">
-                <span className="current-weather-icon"></span>
+                <span className={getWeatherIcon()}></span>
                 <span className="current-temperature">{temperature}<span className="wi wi-celsius"/></span>
                 <span className="current-status">{status}</span>
                 <span className="current-wind-speed">
@@ -29,7 +45,7 @@ class CurrentWeatherTile extends React.Component {
                 </span>
                 <span className="current-pressure">
                     <span className="wi wi-barometer"></span>
-                    Humidity {pressure} hPa
+                    Pressure {pressure} hPa
                 </span>
             </div>
         )
