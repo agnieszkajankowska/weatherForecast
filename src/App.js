70b07234d9413df74ace3310a18ad6c932ec3fcc
Agnieszka Jankowska <ajankowska0404@gmail.com>
2018-05-20 13:07:56 +0200

    Fetching data in app component, displaying data in CurrentWeather component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,31 @@ import ForecastWeatherTile from './components/ForecastWeatherTile';
 
 
 class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            weatherData: null
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://api.openweathermap.org/data/2.5/forecast?q=london,uk&APPID=0de64b18e7da2d5a45857d165125c350')
+            .then(response => response.json()).then(weatherData => this.setState({weatherData: weatherData.list}))
+    }
+
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Weather app</h1>
-                </header>
-                <CurrentWeatherTile/>
-                <ForecastWeatherTile/>
+            <div>
+                {this.state.weatherData === null ? <div className="loader">Loader</div> :
+                    <div className="App">
+                        <header className="App-header">
+                            <h1 className="App-title">Weather app</h1>
+                        </header>
+                        <CurrentWeatherTile weatherData={this.state.weatherData[0]}/>
+                        <ForecastWeatherTile weatherData={this.state.weatherData}/>
+                    </div>
+                }
             </div>
         );
     }
--- src/components/CurrentWeatherTile.js
+++ src/components/CurrentWeatherTile.js
@@ -1,10 +1,36 @@
-import * as React from 'react'
+import * as React from 'react';
+import icons from '../icons'
+import 'weathericons/css/weather-icons.css';
 
 class CurrentWeatherTile extends React.Component {
     render() {
+        const temperature = Math.round(this.props.weatherData.main.temp - 273)
+        const status = this.props.weatherData.weather[0].description
+        const windSpeed = this.props.weatherData.wind.speed
+        const cloudiness = this.props.weatherData.clouds.all
+        const humidity = this.props.weatherData.main.humidity
+        const pressure = this.props.weatherData.main.pressure
         return (
             <div className="current-weather-container">
-                <h1>Current Weather</h1>
+                <span className="current-weather-icon"></span>
+                <span className="current-temperature">{temperature}<span className="wi wi-celsius"/></span>
+                <span className="current-status">{status}</span>
+                <span className="current-wind-speed">
+                    <span className="wi wi-strong-wind"/>
+                    Wind speed {windSpeed}m/s
+                </span>
+                <span className="current-cloudiness">
+                    <span className="wi wi-cloudy"/>
+                    Cloudiness {cloudiness} %
+                </span>
+                <span className="current-smoke">
+                    <span className="wi wi-smoke"/>
+                    Humidity {humidity} %
+                </span>
+                <span className="current-pressure">
+                    <span className="wi wi-barometer"></span>
+                    Humidity {pressure} hPa
+                </span>
             </div>
         )
     }
